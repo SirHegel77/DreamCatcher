0261abcc904a5917179b9e703bdcbf6c771244d0
Heikki Leivo <heikki.leivo@phnet.fi>
2014-03-27 22:45:32 +0200

    Changed recorder to analyze signal power + estimated sleep level.

--- engine/recorder/recorder.py
+++ engine/recorder/recorder.py
@@ -39,7 +39,7 @@ class Recorder(Worker):
             "{0}.motion".format(self._timestamp))
 
 
-    def analyze_axis(self, data, window, dt, breath_power):
+    def analyze_axis(self, data, window, dt):
         mean = np.mean(np.abs(data))
         std = np.std(data)
         logger.info("Mean: %s, std: %s, dt: %s", mean, std, dt)
@@ -47,37 +47,71 @@ class Recorder(Worker):
         spec = np.fft.fft(window)
         freqs = np.fft.fftfreq(window.shape[-1], dt)
         ps = np.abs(spec)**2
-        index = np.argmax(ps)
-        pow = ps[index]
-        if pow > breath_power: 
-            f = np.abs(freqs[index] * 60)
-        else:
-            f = 0
-        logger.info("Max power: %s, freq: %s", pow, f)
-        return [mean, std, f]
+        p = np.log10(np.sum(ps))
+
+        return [mean, std, p]
+
+    def fft(self, w, dt):
+        avg = np.average(w)
+        w = w - avg
+        spec = np.fft.fft(w)
+        spec = spec[0:len(spec)/2]
+        freqs = np.fft.fftfreq(w.shape[-1], dt)
+        freqs = freqs[0:len(freqs)/2]
+        ps = np.abs(spec)**2
+        wl = 5
+        ps = smooth(ps, window_len = wl*2+1)
+        ps = ps[wl:len(ps)-wl]     
+        return (freqs, ps)
+
 
     def analyze(self, timestamps, x, wx, y, wy):
-        config = read_config()
-        breath_power = config.get('recorder', 'breath_power') 
-
         timestamp = long(time.mktime(datetime.now().timetuple()))
         dt = np.abs(np.average(np.gradient(timestamps))) / 1000
-       
-        row = [timestamp] 
-        row += self.analyze_axis(x, wx, dt, breath_power)
-        row += self.analyze_axis(y, wy, dt, breath_power) 
+        (freqs, ps) = self.fft(x, dt)
+        px = np.sum(ps)
+        (freqs, ps) = self.fft(y, dt)
+        py = np.sum(ps)
+        signal_power = np.log10((px + py) / 2)
+ 
+        # Update sleep buffer
+        power_min = self._config.getfloat('recorder', 'power_min')
+        power_max = self._config.getfloat('recorder', 'power_max')
+        delta_trigger = self._config.getfloat('recorder', 'delta_trigger')
+        level_trigger = self._config.getfloat('recorder', 'level_trigger')
+        if signal_power > power_max:
+            value = np.ones(1, dtype='f')
+        elif signal_power < power_min:
+            value = np.ones(1, dtype='f')
+        else:
+            value = np.ones(1, dtype='f') * -1.0
+        self._sleep_window.extend(value)
+        sleep_level = np.average(
+            self._sleep_window.get())
+  
+        delta = sleep_level - self._sleep_level 
+
+        if delta > delta_trigger:
+            if self._sleep_level < level_trigger:
+                if sleep_level > level_trigger:
+                    self.record_marker(color='r', 
+                        comment='Sleep level trigger')
+
+        self._sleep_level = sleep_level
+
+        row = [timestamp, signal_power, sleep_level, delta] 
+        logger.info("Analyzed %s", row)
 
         with open(self.data_filename, "a") as f:
             f.write(';'.join((str(x) for x in row)) + '\n')
-
-        if config.getboolean('recorder', 'emulate') == False:
+        if self._config.getboolean('recorder', 'emulate') == False:
             filename = os.path.join(self._npzdir, 
                     "{0}.npz".format(timestamp))
             np.savez(filename, dt = dt, timestamps = timestamps, 
                 x = x, y = y, wx = wx, wy = wy)
 
-    def emulate(self, config):
-        timestamp = config.get('recorder', 'emulator_timestamp')
+    def emulate(self):
+        timestamp = self._config.get('recorder', 'emulator_timestamp')
         datafile = os.path.join(self._directory, timestamp + '.data')
         npzdir = os.path.join(self._directory, timestamp)
         with open(datafile, 'r') as f:
@@ -92,8 +126,8 @@ class Recorder(Worker):
                     yield [t[i], x[i], y[i]]
                 
 
-    def record_gyro(self, config):
-        minimu_command = config.get('minimu', 'command').split()
+    def record_gyro(self):
+        minimu_command = self._config.get('minimu', 'command').split()
         with Process(minimu_command) as p:
             for line in p:
                 if self._should_stop:
@@ -105,24 +139,23 @@ class Recorder(Worker):
                 yield [t, x, y]
 
     def record(self):
-        config = read_config()
-        if config.getboolean('recorder', 'emulate'):
+        if self._config.getboolean('recorder', 'emulate'):
             recorder = self.emulate
         else:
             recorder = self.record_gyro
-
         logger.info("Recording gyro...")
-        window_length = config.getint('recorder', 'window_length')
-        increment = config.getint('recorder', 'window_increment')
+        window_length = self._config.getint('recorder', 'window_length')
+        increment = self._config.getint('recorder', 'window_increment')
         wx = RingBuffer(window_length)
         wy = RingBuffer(window_length)
+	self._sleep_window = RingBuffer(
+            self._config.getint('recorder', 'sleep_window_length'))
+        self._sleep_level = 0.0
         timestamps = np.zeros(increment, dtype='f')
         x = np.zeros(increment, dtype='f')
         y = np.zeros(increment, dtype='f')
-        mean_x = config.getfloat('recorder', 'mean_x')
-        mean_y = config.getfloat('recorder', 'mean_y')
         i = 0
-        for t, rx, ry in recorder(config):
+        for t, rx, ry in recorder():
             if self._should_stop:
                 break
             x[i] = rx
@@ -142,16 +175,15 @@ class Recorder(Worker):
         dts = [t[i+1]-t[i] for i in range(len(t)-1)]
         return np.average(np.array(dts)) 
 
-    def record_marker(self):
-        logger.info("Recording marker")
+    def record_marker(self, color='k', comment='Manually triggered'):
+        logger.info("Recording marker, color = %s", color)
         timestamp = long(time.mktime(datetime.now().timetuple()))
         with open(self.marker_filename, "a") as f:
-            f.write('{0};b\n'.format(timestamp))
+            f.write(';'.join([str(timestamp), color, comment]) + '\n')
 
     def record_motion(self, motion_data):
         logger.info("Recording motion data.")
-        config = read_config()
-        session = config.getint('recorder', 'current_session')
+        session = self._config.getint('recorder', 'current_session')
         if session == 0:
             logger.error("No active session.")
             return
@@ -167,9 +199,9 @@ class Recorder(Worker):
         import numpy as np
         logger.info("Recording to %s", self.data_filename)
 
-        config = read_config()
-        config.set('recorder', 'current_session', str(self._timestamp))
-        save_config(config)
+        self._config = read_config()
+        self._config.set('recorder', 'current_session', str(self._timestamp))
+        save_config(self._config)
         try:
             self._worker.start()
             with open(self.marker_filename, "w") as f:
@@ -183,7 +215,7 @@ class Recorder(Worker):
             logger.error("Unhandled exception: %s", sys.exc_info()[1])
         finally:
             self._worker.stop()
-            config.set('recorder', 'current_session',str(0))
-            save_config(config)
+            self._config.set('recorder', 'current_session',str(0))
+            save_config(self._config)
         logger.info("Finished recording")
 
